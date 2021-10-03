b39722954613903ff7eee39be0810a41b6097249
Stefan Laun <sl@shoprocket.de>
2021-10-03 17:18:59 +0200

    Maus hinzugefügt auf index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-echo "<p>Hello, my name ist Stefan and I am a happy mouse</p><br>";
+echo "<p>Hello, my name is Stefan and I am a happy mouse</p><br>";
 echo "<img src='https://res-launio.fra1.digitaloceanspaces.com/Anonymice-3701.png'>";
+echo "<p>Visit also my <a href='https://nfts.laun.io'>online exhibition</a> for some more NFTs.</p>"
