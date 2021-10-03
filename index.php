a85d4b68584e21d34b344f4bb0a7116605ef2a28
Stefan Laun <sl@shoprocket.de>
2021-10-03 17:13:08 +0200

    Maus hinzugefügt auf index.php

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php
-echo "hello world";
+echo "<p>Hello, my name ist Stefan and I am a happy mouse</p><br>";
+echo "<img src='https://res-launio.fra1.digitaloceanspaces.com/Anonymice-3701.png'>";
